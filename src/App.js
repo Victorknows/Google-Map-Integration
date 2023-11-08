b70f08712f48f0ccf88f6f2764f22bc3fbdfc800
Gathoni <janetgathoni910@gmail.com>
2023-11-08 22:00:44 +0300

    Markers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl'; 
+import mapboxgl from 'mapbox-gl';
 function App() {
   mapboxgl.accessToken = 'pk.eyJ1Ijoibm9ubmllLWdhdGhvbmkiLCJhIjoiY2xvbzlwNXYxMzNqeDJscndxbmI2eTkzMCJ9.BbIACXxO5Dye7k4gZLgz-g';
 
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng, setLng] = useState(36.817223);
-  const [lat, setLat] = useState( -1.286389);
+  const [lat, setLat] = useState(-1.286389);
   const [zoom, setZoom] = useState(9);
 
   useEffect(() => {
@@ -17,16 +17,22 @@ function App() {
       center: [lng, lat],
       zoom: zoom
     });
+    
+    // // Create a new marker.
+    const marker = new mapboxgl.Marker()
+      .setLngLat([-1.290394, 36.816583])
+      .addTo(map);
+    // Set marker options.
+
   });
 
-  
-    return (
-      <div>
-        <div ref={mapContainer} className="map-container" />
-      </div>
-    );
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
 
-  
+
 }
 
 export default App;
