946c9606904c7ca09bb115263d62fcc91ef6a0a4
Gathoni <janetgathoni910@gmail.com>
2023-11-13 21:06:25 +0300

    Token

---
+++ src/Adress.js
@@ -0,0 +1,37 @@
+import React from 'react'; 
+import { AddressAutofill } from '@mapbox/search-js-react';
+
+const Address = () => {
+    return (
+        <form>
+            <AddressAutofill accessToken="1pk.eyJ1Ijoibm9ubmllLWdhdGhvbmkiLCJhIjoiY2xvbzlwNXYxMzNqeDJscndxbmI2eTkzMCJ9.BbIACXxO5Dye7k4gZLgz-g">
+                <input
+                    name="address" placeholder="Address" type="text"
+                    autoComplete="address-line1"
+                />
+            </AddressAutofill>
+            <input
+                name="apartment" placeholder="Apartment number" type="text"
+                autoComplete="address-line2"
+            />
+            <input
+                name="city" placeholder="City" type="text"
+                autoComplete="address-level2"
+            />
+            <input
+                name="state" placeholder="State" type="text"
+                autoComplete="address-level1"
+            />
+            <input
+                name="country" placeholder="Country" type="text"
+                autoComplete="country"
+            />
+            <input
+                name="postcode" placeholder="Postcode" type="text"
+                autoComplete="postal-code"
+            />
+        </form>
+    );
+};
+
+export default Address;
--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
+// import React, { useRef, useEffect, useState } from 'react';
+// import mapboxgl from 'mapbox-gl';
+import Adress from './Adress';
 function App() {
-  mapboxgl.accessToken = 'pk.eyJ1Ijoibm9ubmllLWdhdGhvbmkiLCJhIjoiY2xvbzlwNXYxMzNqeDJscndxbmI2eTkzMCJ9.BbIACXxO5Dye7k4gZLgz-g';
+  // mapboxgl.accessToken = 'pk.eyJ1Ijoibm9ubmllLWdhdGhvbmkiLCJhIjoiY2xvbzlwNXYxMzNqeDJscndxbmI2eTkzMCJ9.BbIACXxO5Dye7k4gZLgz-g';
 
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(36.817223);
-  const [lat, setLat] = useState(-1.286389);
-  const [zoom, setZoom] = useState(9);
+  // const mapContainer = useRef(null);
+  // const map = useRef(null);
+  // const [lng, setLng] = useState(36.817223);
+  // const [lat, setLat] = useState(-1.286389);
+  // const [zoom, setZoom] = useState(9);
 
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom: zoom
-    });
+  // useEffect(() => {
+  //   if (map.current) return; // initialize map only once
+  //   map.current = new mapboxgl.Map({
+  //     container: mapContainer.current,
+  //     style: 'mapbox://styles/mapbox/streets-v12',
+  //     center: [lng, lat],
+  //     zoom: zoom
+  //   });
     
-    // // Create a new marker.
-    const marker = new mapboxgl.Marker()
-      .setLngLat([-1.290394, 36.816583])
-      .addTo(map);
-    // Set marker options.
+  //   // Create default markers
+  //   // map.current((feature) =>
+      
+  //   // );
+  //   // new mapboxgl.Marker().setLngLat(getCoordinates("SRID=4326;POINT (-9.419645 7.4856489)")).addTo(map)
 
-  });
+  //   console.log(getCoordinates("SRID=4326;POINT (-9.419645 7.4856489)"))
+  //   return () => map.remove();
+  // }, []);
+  // function getCoordinates(point) {
+
+  //   let a = point.indexOf("(")
+  //   let b = point.lastIndexOf(")")
+  //   let d = point.slice(a + 1, b)
+  //   let co = d.split(" ").map(l=> {
+  //     return parseFloat(l)
+  //   })
+
+  //   return co
+  // }
+
+ 
 
   return (
     <div>
-      <div ref={mapContainer} className="map-container" />
+    {/* //   <div ref={mapContainer} className="map-container" /> */}
+      <Adress/>
+  
     </div>
   );
 
